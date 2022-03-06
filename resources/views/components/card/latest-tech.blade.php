0582cb054fb5a5e8c70afed449424065cb51705e
Jakub <jakubmoz003@gmail.com>
2022-03-05 19:18:11 -0600

    setup latest tools section

---
+++ resources/views/components/card/latest-tech.blade.php
@@ -0,0 +1,12 @@
+@props([
+    'name' => 'Zapier',
+    'description' => 'Connnect 1000+ Apps',
+])
+<a {{ $attributes }} href="#"
+    class="rounded-lg py-10 flex flex-col items-center justify-center shadow-lg border border-gray-100">
+    <div class="w-16 h-auto">
+        {{ $slot }}
+    </div>
+    <p class=" font-bold mt-4">{{ $name }}</p>
+    <p class="mt-2 text-sm text-gray-500">{{ $description }}</p>
+</a>
