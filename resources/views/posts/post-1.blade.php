aa8f90342c356bdc24efcaceb71635f7adcb012c
Jakub <jakubmoz003@gmail.com>
2022-03-07 18:27:53 -0600

    adjusted navigation

--- resources/views/posts/post-1.blade.php
+++ resources/views/posts/post-1.blade.php
@@ -1,5 +1,9 @@
+---
+title: Post 1
+excerpt: Lorem ipsum dolor sit amet consectetur adipisicing elit.
+date: 2021-05-21
+---
 <div class="prose-lg text-slate-800">
-    <h3>Post One</h3>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur omnis eos beatae, optio non minus voluptatum
         ipsam, commodi assumenda illum sunt, blanditiis natus voluptates iusto amet rem at. Eveniet, necessitatibus.</p>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur omnis eos beatae, optio non minus voluptatum
--- routes/web.php
+++ routes/web.php
@@ -5,12 +5,11 @@ use App\Models\CheatSheet;
 
 
 Route::get('/', function () { return view('pages/home'); })->name('home');
-Route::get('/roadmap', function () { return view('pages/home'); })->name('roadmap');
+Route::get('/roadmap', function () { return view('pages/roadmap'); })->name('roadmap');
 Route::get('/components', function () { return view('pages/home'); })->name('components');
 Route::get('/demos', function () { return view('pages/home'); })->name('demos');
 
 Route::get('/cheat-sheet', function () {
-
     $articles = CheatSheet::all();
 
     return view('pages/cheat-sheet', [
