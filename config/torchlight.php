21261bff2ace857f2736a6f74eebda8967839fd9
Jakub <jakubmoz003@gmail.com>
2022-03-09 19:24:44 -0600

    update to linenumbers style in torchlight

--- config/torchlight.php
+++ config/torchlight.php
@@ -50,7 +50,7 @@ return [
         'lineNumbers' => true,
 
         // Control the `style` attribute applied to line numbers.
-        // 'lineNumbersStyle' => '',
+        'lineNumbersStyle' => '',
 
         // Turn on +/- diff indicators.
         // 'diffIndicators' => true,
