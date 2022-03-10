982f085a041f25b5034ac4ad537fa9049896d7c4
Jakub <jakubmoz003@gmail.com>
2022-03-09 18:57:11 -0600

    test

--- resources/views/components/template/article.blade.php
+++ resources/views/components/template/article.blade.php
@@ -7,7 +7,7 @@
             @include('pages.sidebar')
         </section>
         <section class="w-[640px] flex flex-col space-y-10">
-            <div id="report" class="prose">
+            <div id="report">
                 <h2 class="text-4xl font-bold">{!! $title !!}</h2>
             </div>
             <div class=" flex flex-col space-y-10">
