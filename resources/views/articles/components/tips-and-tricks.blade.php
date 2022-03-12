03a31ca9b645cfd621316f78e9a76fc74f6424da
Jakub <jakubmoz003@gmail.com>
2022-03-12 15:16:35 -0600

    installed markdown

--- resources/views/articles/components/tips-and-tricks.blade.php
+++ resources/views/articles/components/tips-and-tricks.blade.php
@@ -1,8 +1,16 @@
 <x-template.article title="Tips and Tricks">
     <x-slot name="content">
-        <div>
-            Artisan Commands
-        </div>
+        @markdown
+        ## Bar
+        https://spatie.be/docs/laravel-markdown/v1/introduction
+        - bullet points at
+
+        ```php
+        <a href="#hype-rating" class="text-sm text-slate-400  hover:text-slate-700 hover:font-bold font-medium">
+            Hype Rating
+        </a>
+        ```
+        @endmarkdown
     </x-slot>
 
     <x-slot name="navigation">
