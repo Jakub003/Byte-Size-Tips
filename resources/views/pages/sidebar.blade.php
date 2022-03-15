5b8bf6d9544e793a4200fb00c76eeb1cf736e508
Jakub <jakubmoz003@gmail.com>
2022-03-15 16:20:20 -0500

    updated button-menu items for sidebar

--- resources/views/pages/sidebar.blade.php
+++ resources/views/pages/sidebar.blade.php
@@ -3,8 +3,8 @@
         <h2 class="text-sm text-slate-300 font-bold uppercase">Getting Started</h2>
         @foreach ($gettingStartedArticles as $article)
             {{-- @dd($article) --}}
-            <x-button.menu-item name="{{ rtrim($article->getFilename(), '.md') }}" route="getting-started"
-                url="{{ rtrim($article->getFilename(), '.md') }}" />
+            <x-button.menu-item name="{{ str_replace('-', ' ', rtrim($article->getFilename(), '.md')) }}"
+                route="getting-started" url="{{ rtrim($article->getFilename(), '.md') }}" />
         @endforeach
 
 
@@ -12,27 +12,30 @@
     <div class="flex flex-col space-y-1.5">
         <h2 class="text-sm text-slate-300 font-bold uppercase">Cheat Sheet</h2>
         @foreach ($cheatSheetArticles as $article)
-            <x-button.menu-item name="Introduction" route="getting-started" url="introduction" />
+            <x-button.menu-item name="{{ str_replace('-', ' ', rtrim($article->getFilename(), '.md')) }}"
+                route="getting-started" url="{{ rtrim($article->getFilename(), '.md') }}" />
         @endforeach
     </div>
 
     <div class="flex flex-col space-y-1.5">
         <h2 class="text-sm text-slate-300 font-bold uppercase">Features</h2>
         @foreach ($featureArticles as $article)
-            <x-button.menu-item name="{{ rtrim($article->getFilename(), '.md') }}" route="features"
-                url="{{ rtrim($article->getFilename(), '.md') }}" />
+            <x-button.menu-item name="{{ str_replace('-', ' ', rtrim($article->getFilename(), '.md')) }}"
+                route="features" url="{{ rtrim($article->getFilename(), '.md') }}" />
         @endforeach
     </div>
     <div class="flex flex-col space-y-1.5">
         <h2 class="text-sm text-slate-300 font-bold uppercase">Components</h2>
         @foreach ($componentArticles as $article)
-            <x-button.menu-item name="Introduction" route="getting-started" url="introduction" />
+            <x-button.menu-item name="{{ str_replace('-', ' ', rtrim($article->getFilename(), '.md')) }}"
+                route="getting-started" url="{{ rtrim($article->getFilename(), '.md') }}" />
         @endforeach
     </div>
     <div class="flex flex-col space-y-1.5">
         <h2 class="text-sm text-slate-300 font-bold uppercase">SASS Roadmap</h2>
         @foreach ($roadmapArticles as $article)
-            <x-button.menu-item name="Introduction" route="getting-started" url="introduction" />
+            <x-button.menu-item name="{{ str_replace('-', ' ', rtrim($article->getFilename(), '.md')) }}"
+                route="getting-started" url="{{ rtrim($article->getFilename(), '.md') }}" />
         @endforeach
     </div>
 
